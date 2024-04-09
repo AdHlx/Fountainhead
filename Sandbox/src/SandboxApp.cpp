6139efb62e80f21c626b32441e6d3165fef9e4a9
AdHlx <1418753102@qq.com>
2024-04-09 19:15:43 +0800

    Added transforms to Renderer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,9 @@
 #include <Fountainhead.h>
 
 #include "imgui/imgui.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Fountainhead::Layer
 {
 public:
@@ -32,10 +35,10 @@ public:
 		m_SquareVA.reset(Fountainhead::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-				-0.75f, -0.75f, 0.0f,
-				 0.75f, -0.75f, 0.0f,
-				 0.75f,  0.75f, 0.0f,
-				-0.75f,  0.75f, 0.0f
+				-0.5f, -0.5f, 0.0f,
+				 0.5f, -0.5f, 0.0f,
+				 0.5f,  0.5f, 0.0f,
+				-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Fountainhead::VertexBuffer> squareVB;
@@ -57,6 +60,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -65,7 +69,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -92,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -144,7 +149,19 @@ public:
 
 		Fountainhead::Renderer::BeginScene(m_Camera);
 
-		Fountainhead::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Fountainhead::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
+
 		Fountainhead::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Fountainhead::Renderer::EndScene();
